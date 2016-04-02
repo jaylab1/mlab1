eddad8759c871ca5301d4439ac74dd5ddf1d75ab
Medialab LEB <medialablebanon@gmail.com>
2016-04-02 14:02:23 +0300

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -93,7 +93,7 @@ document.addEventListener("deviceready", function() {
         });
         
         map.on(plugin.google.maps.event.MY_LOCATION_BUTTON_CLICK, function (position) {
-        	//alert(JSON.stringify(position));
+        	alert(JSON.stringify(position));
         	map.setCenter(setPosition(position.target.lat, position.target.lng));
         	marker.setPosition(setPosition(position.target.lat, position.target.lng));
         	map.refreshLayout();
