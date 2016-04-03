cb27bd0536137ba32cc9d7473aa67c8cd929ed3a
Medialab LEB <medialablebanon@gmail.com>
2016-04-04 00:17:57 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -56,26 +56,7 @@ document.addEventListener("deviceready", function() {
   			
 	},2000);*/
 
-var points = [
-  new plugin.google.maps.LatLng(41.79883, 140.75675),
-  new plugin.google.maps.LatLng(41.799240000000005, 140.75875000000002),
-  new plugin.google.maps.LatLng(41.797650000000004, 140.75905),
-  new plugin.google.maps.LatLng(41.79637, 140.76018000000002),
-  new plugin.google.maps.LatLng(41.79567, 140.75845),
-  new plugin.google.maps.LatLng(41.794470000000004, 140.75714000000002),
-  new plugin.google.maps.LatLng(41.795010000000005, 140.75611),
-  new plugin.google.maps.LatLng(41.79477000000001, 140.75484),
-  new plugin.google.maps.LatLng(41.79576, 140.75475),
-  new plugin.google.maps.LatLng(41.796150000000004, 140.75364000000002),
-  new plugin.google.maps.LatLng(41.79744, 140.75454000000002),
-  new plugin.google.maps.LatLng(41.79909000000001, 140.75465),
-  new plugin.google.maps.LatLng(41.79883, 140.75673)
-];
-var latLngBounds = new plugin.google.maps.LatLngBounds(points);
 
-map.animateCamera({
-  'target' : latLngBounds
-});
             
           
 	map.on(plugin.google.maps.event.CAMERA_CHANGE, function () {
@@ -87,7 +68,21 @@ map.animateCamera({
 		},7000);*/
 		
     	});
-
+    	
+	map.on(plugin.google.maps.event.MAP_CLICK, function () {
+		
+    		map.getCameraPosition(function(camera) {
+    		
+  		var buff = ["Current camera position:\n",
+      		"latitude:" + camera.target.lat,
+      		"longitude:" + camera.target.lng,
+      		"zoom:" + camera.zoom,
+      		"tilt:" + camera.tilt,
+        	"bearing:" + camera.bearing].join("\n");
+        	
+  		alert(buff);
+       		});
+       });
 
     }, onLocationError, option);
 	//END NAVIGATOR ////////////////////////////////////////////////////////////////////////////  
