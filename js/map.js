4c9947980b417d23f9e355f47155fd498787e9b1
Medialab LEB <medialablebanon@gmail.com>
2016-04-04 00:04:23 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -36,7 +36,7 @@ document.addEventListener("deviceready", function() {
         map.setOptions({
             'camera': {
                 'latLng': location,
-                'zoom': 5,
+                'zoom': 10,
                 'bearing': 50,
                 'duration': 15000 // 10 seconds
             },
@@ -44,7 +44,7 @@ document.addEventListener("deviceready", function() {
         });
         
        
-	setTimeout(function(){
+	/*setTimeout(function(){
   			
             map.animateCamera({
                 'zoom': 14,
@@ -54,9 +54,28 @@ document.addEventListener("deviceready", function() {
             ONSTOP();
   			
   			
-	},2000);
+	},2000);*/
 
+var points = [
+  new plugin.google.maps.LatLng(41.79883, 140.75675),
+  new plugin.google.maps.LatLng(41.799240000000005, 140.75875000000002),
+  new plugin.google.maps.LatLng(41.797650000000004, 140.75905),
+  new plugin.google.maps.LatLng(41.79637, 140.76018000000002),
+  new plugin.google.maps.LatLng(41.79567, 140.75845),
+  new plugin.google.maps.LatLng(41.794470000000004, 140.75714000000002),
+  new plugin.google.maps.LatLng(41.795010000000005, 140.75611),
+  new plugin.google.maps.LatLng(41.79477000000001, 140.75484),
+  new plugin.google.maps.LatLng(41.79576, 140.75475),
+  new plugin.google.maps.LatLng(41.796150000000004, 140.75364000000002),
+  new plugin.google.maps.LatLng(41.79744, 140.75454000000002),
+  new plugin.google.maps.LatLng(41.79909000000001, 140.75465),
+  new plugin.google.maps.LatLng(41.79883, 140.75673)
+];
+var latLngBounds = new plugin.google.maps.LatLngBounds(points);
 
+map.animateCamera({
+  'target' : latLngBounds
+});
             
           
 	map.on(plugin.google.maps.event.CAMERA_CHANGE, function () {
