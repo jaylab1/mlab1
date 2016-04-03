163369dcd900f2c02de1da6c7fa1993d5384c3b6
Medialab LEB <medialablebanon@gmail.com>
2016-04-03 12:43:19 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,99 +1,84 @@
-//CONNECTING MAP
-
-function showLocation(position) {
-    
-            var latitude = position.coords.latitude;
-            var longitude = position.coords.longitude;
-            alert("Latitude : " + latitude + " Longitude: " + longitude);
-            
-return new plugin.google.maps.LatLng(latitude,longitude);        
-}
-
-function errorHandler(err) {
-    
-            if(err.code == 1) {
-               alert("Error: Access is denied!");
-            }
-            
-            else if( err.code == 2) {
-               alert("Error: Position is unavailable!");
-            }
-}
-			
-function getLocation(){
-
-            if(navigator.geolocation){
-               // timeout at 60000 milliseconds (60 seconds)
-               var options = {
-                
-                    enableHighAccuracy: true,      // Force GPS
-                    timeout: 5000,
-                    maximumAge: 3000
-                    
-                };
-                
-               navigator.geolocation.getCurrentPosition(showLocation, errorHandler, options);
-            }
-            
-            else{
-               alert("Sorry, browser does not support geolocation!");
-            }
-}
-
-
 // Add Google Native Map
 document.addEventListener("deviceready", function() {
-    
-    // Getting the map selector in DOM
+  // Getting the map selector in DOM
     var div = document.getElementById("map_canvas");
-    
     var option = {
-        enableHighAccuracy: true, // Force GPS
+        enableHighAccuracy: true,      // Force GPS
         timeout: 5000,
         maximumAge: 3000
     };
 
     var map = plugin.google.maps.Map.getMap(div, {
-        'controls': {
-            'compass': false,
-            'zoom': false,
-            'myLocationButton': true,
-            'indoorPicker': true
-        },
-        'gestures': {
-            'scroll': true,
-            'tilt': true,
-            'rotate': true,
-            'zoom': true
-        }
-    });
+            'controls': {
+                
+                'compass': false,
+                'zoom': false,
+                'myLocationButton': true,
+    			'indoorPicker': true
+                
+            },
+            
+            'gestures': {
+                'scroll': true,
+                'tilt': true,
+                'rotate': true,
+                'zoom': true
+            }
+            
+        });
+        
+        
+    //NAVIGATOR ON
+    navigator.geolocation.getCurrentPosition(function (position) {
+        
+        
+        var location = setPosition(position.coords.latitude, position.coords.longitude);
 
-            
-            
         map.setOptions({
             'camera': {
-                'latLng': getLocation(),
+                'latLng': location,
                 'zoom': 14,
                 'bearing': 50
             },
-            'center': setPosition(position.coords.latitude, position.coords.longitude)
+            'center': location
         });
+        //map.setPadding(0, 0, 0);
 
 
+        // Capturing event when Map load are ready.
+        /*map.addEventListener(plugin.google.maps.event.MAP_READY, function(){
+            
+           
+           
+            map.animateCamera({
+                'target': location,
+                'zoom': 14,
+                'bearing': 140
+            });
 
-     map.on(plugin.google.maps.event.MY_LOCATION_BUTTON_CLICK, function() {
+            //map.setCenter(location);
+            //marker.setMap(map); */
+            //map.refreshLayout();
+        });*/
+        
+    }, onLocationError, option);
+	///END NAVIGATOR ON////////////////////////////////////////////////////////////////////////
 
+	map.on(plugin.google.maps.event.MY_LOCATION_BUTTON_CLICK, function (position) {
+		console.log(position);
     });
+
+    // Function that return a LatLng Object to Map
+    function setPosition(lat, lng) {
+        return new plugin.google.maps.LatLng(lat, lng);
+    }
     
     
-	
-	
 });
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
-
-$("#map_canvas").on('touchstart click', function(e){
-  e.preventDefault();
-  alert("test");
-});
+function onLocationError( error_msg ) {
+    alert("Plase enable GPS location service.\n" + JSON.stringify(error_msg, null, 4));
+}
