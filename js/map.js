b8ccb496467be61a25bf01f9eecbe0abf6217a2d
Medialab LEB <medialablebanon@gmail.com>
2016-04-06 23:52:38 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -15,6 +15,8 @@ document.addEventListener("deviceready", function() {
   				//FETCH THE lnglng from server and get back with address
   				var geocode = position.coords.latitude+','+position.coords.longitude;
   				
+  				alert(geocode);
+  				
   				SENDget(
   					'get-geoaddress',
   					'geocode='+geocode,
