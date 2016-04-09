46fb53bf357b0c81b7c235d2c49c643c21879896
Medialab LEB <medialablebanon@gmail.com>
2016-04-09 12:23:04 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -74,7 +74,9 @@ document.addEventListener("deviceready", function() {
     map.addMarker({
        'position': new plugin.google.maps.LatLng(33.8730011,35.5179532),
        'title' : "Hello GoogleMap for Cordova!",
-       'icon'  : 'www/img/pinhail-car-small.png'
+       'icon'  : {
+    		'url': 'www/img/pinhail-car-small.png'
+   	}
     });
 
   });
