62a55286a59a147b14299439a2e63c757f2bee18
Medialab LEB <medialablebanon@gmail.com>
2016-04-13 16:20:32 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -324,18 +324,19 @@ function GETcoord(lat,long,mode='0'){
 
 			
 		addMarkers(map,jsonObj, function(markers) {
-					
+				
+				var result;	
   				//markers[0].showInfoWindow();
   				setTimeout(function(){ 
   				
   					for (var i = 0; i < markers.length; i++) {
   					
-  					//translate = items['location'].split(","); 
-  					localStorage.setItem('MARKER1',markers[i].get("position"));
-					var result = [33.8836142,35.5303557];
-    	    				transition(result,markers[i]); 
-    	    				alert(i);
-  						
+  						//translate = items['location'].split(","); 
+  						localStorage.setItem('MARKER1',markers[i].get("position"));
+						result = [33.8836142,35.5303557];
+    	    					transition(result,markers[i]); 
+    	    					
+    	    					
   					}
   					
   				}, 7000); 
