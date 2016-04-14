b4dd098361b61b8266eba7042b86120fc47cf2c7
Medialab LEB <medialablebanon@gmail.com>
2016-04-15 01:01:56 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,51 +1,11 @@
-/**
- * For debug purpose, catch JavaScript errors.
- */
-window.onerror = function(message, file, line) {
-  var error = [];
-  error.push('---[error]');
-  if (typeof message == "object") {
-    var keys = Object.keys(message);
-    keys.forEach(function(key) {
-      error.push('[' + key + '] ' + message[key]);
-    });
-  } else {
-    error.push(line + ' at ' + file);
-    error.push(message);
-  }
-  alert(error.join("\n"));
-};
-
     var map;
     document.addEventListener("deviceready", function() {
       var div = document.getElementById("map_canvas");
       // Initialize the map view
-      map = plugin.google.maps.Map.getMap(div,{
-	   
-  		'backgroundColor': 'white',
-  		'controls': {
-    			'myLocationButton': true,
-    			'compass': false,
-  		},
-		'gestures': {
-    			'scroll': true,
-    			'tilt': true,
-    			'rotate': true,
-    			'zoom': true
-  		},
-            	'camera': {
-                	'latLng': new plugin.google.maps.LatLng(33.8886459,35.4867246),
-                	'zoom': 10,
-                	'bearing': 140,
-                
-            	},
-            	'center': location
-      }); 
+      map = plugin.google.maps.Map.getMap(div);
       // Wait until the map is ready status.
       map.addEventListener(plugin.google.maps.event.MAP_READY, onMapReady);
     }, false);
-    
-    
     function onMapReady() {
       var button = document.getElementById("button");
       button.addEventListener("click", onBtnClicked, false);
