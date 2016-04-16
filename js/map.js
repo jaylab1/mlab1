915ea8423ba84882dd3ea21b6c629f29fa9951a9
Medialab LEB <medialablebanon@gmail.com>
2016-04-16 16:52:49 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -105,6 +105,8 @@ window.onerror = function(message, file, line) {
                	
                	      i = 0;
                	      
+               	     alert("MARKER"+marker+"from1"+from1,"from2"+from2+"to1"+to1+"to2"+to2);
+               	      
                	      deltaLat = (to1 - from1)/numDeltas;
         	      deltaLng = (to2 - from2)/numDeltas;
         	      
@@ -118,6 +120,8 @@ window.onerror = function(message, file, line) {
                
                function INTanimation(from1,from2,deltaLat,deltaLng,marker){
                	
+        	      	alert("i"+i+"--ANIMMARKER"+marker+"from1"+from1,"from2"+from2+"to1"+to1+"to2"+to2);
+        	      	
         	      	var latlng = new plugin.google.maps.LatLng(from1, from2);
         	      	marker.setPosition(latlng);
                	      
@@ -163,6 +167,8 @@ window.onerror = function(message, file, line) {
         				
     		        });
       			if (markers.length === data.length) {
+      				
+      				alert("MARKER PLACE":+JSON.stringify(coord));
       				
       				$.each(coord, function(k,items){
       					
